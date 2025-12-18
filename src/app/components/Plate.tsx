6e43462269ef7317e90617441f74946c2e605dd4
Brenden Mersey <brenden@daymarker.digital>
2025-12-18 14:27:10 -0800

    Structural update to the Plate.tsx component and removal of un-needed PlateWeightMarker.tsx component.

--- src/app/components/Plate.tsx
+++ src/app/components/Plate.tsx
@@ -1,5 +1,5 @@
 "use client";
-import PlateWeightMarker from "./PlateWeightMarker";
+
 import { PLATE_COLOURS } from "../constants/plates";
 
 type PlateProps = {
@@ -14,31 +14,25 @@ export default function Plate({ weight, count, onAdd, onRemove }: PlateProps) {
   return (
     <div className="plate">
       <button
-        className="plate__button-add button"
+        className="plate__button button"
         onClick={onAdd}
         data-weight={weight}
         data-colour={plateColour}
       >
-        <div className="plate__circle-container">
-          <span className="plate__circle-big"></span>
-          <span className="plate__circle-small"></span>
-          <span className="plate__circle-hole"></span>
-        </div>
-        <div className="plate__content-container">
-          <PlateWeightMarker weight={weight} side="left" />
-          <PlateWeightMarker weight={weight} side="right" />
+        <div className="plate__weight-marker">
+          <span className="plate__weight">{weight}</span>
+          <span className="plate__unit">LB</span>
         </div>
       </button>
 
       {count > 0 && <span className="plate__count">×{count}</span>}
 
-      <button
-        className="plate__button-remove button"
-        onClick={onRemove}
-        disabled={count === 0}
-      >
-        <span className="plate-cell__remove-icon">-</span>
-      </button>
+      <div className="plate__controls">
+        <button onClick={onAdd}>+</button>
+        <button onClick={onRemove} disabled={count === 0}>
+          -
+        </button>
+      </div>
     </div>
   );
 }
